64a441a587ceb625cf2de161b56df6099a412f8c
Dawid Dziurla <dawidd0811@gmail.com>
2018-07-21 14:19:39 +0200

    simplify some settings stuff

--- app/src/main/java/com/github/dawidd6/andttt/SettingsFragment.java
+++ app/src/main/java/com/github/dawidd6/andttt/SettingsFragment.java
@@ -1,36 +1,17 @@
 package com.github.dawidd6.andttt;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.widget.ListView;
 
-public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener {
+public class SettingsFragment extends PreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
-        findPreference("licenses").setOnPreferenceClickListener(this);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        // remove dividers
-        ListView list = getView().findViewById(android.R.id.list);
-        list.setDivider(null);
-    }
-
-    @Override
-    public boolean onPreferenceClick(Preference preference) {
-        switch(preference.getKey()) {
-            case "licenses":
-                Intent intent = new Intent(getActivity(), LicensesActivity.class);
-                startActivity(intent);
-                break;
-        }
-        return true;
     }
 }

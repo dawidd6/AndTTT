e377ae22527bc9785a33f5fc804a329019906368
Dawid Dziurla <dawidd0811@gmail.com>
2018-11-05 17:11:38 +0100

    dialogs: add base dialog

---
+++ app/src/main/java/com/github/dawidd6/andttt/dialogs/BaseDialogFragment.java
@@ -0,0 +1,22 @@
+package com.github.dawidd6.andttt.dialogs;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.github.dawidd6.andttt.MainActivity;
+
+import java.util.Objects;
+
+public class BaseDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        if(((MainActivity) getActivity()).isStatusBarEnabled()) {
+            Objects.requireNonNull(dialog.getWindow()).clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            Objects.requireNonNull(dialog.getWindow()).addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        return dialog;
+    }
+}

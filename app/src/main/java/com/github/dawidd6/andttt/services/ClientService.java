3939174f4780fe2c2641985513778fc1e2913612
Dawid Dziurla <dawidd0811@gmail.com>
2021-01-26 12:36:16 +0100

    service: import context

--- app/src/main/java/com/github/dawidd6/andttt/services/ClientService.java
+++ app/src/main/java/com/github/dawidd6/andttt/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.github.dawidd6.andttt.services;
 
+import android.content.Context;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
